e89bdf87372757e4dbcdf447f25c0de1aff371e8
moony1529 <104741605+moony1529@users.noreply.github.com>
2022-10-30 14:04:10 +0800

    Create 顺序表按位查找(动态).c

---
+++ 数据结构/线性表/顺序表按位查找(动态).c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MaxSize 10
+typedef struct {
+	int* data;
+	int Lengeth;
+}SqList;
+void InitList(SqList& L, int n)
+{
+	L.data = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++)
+	{
+		L.data[i] = i;
+	}
+	L.Lengeth = n;
+}
+//顺序表的查找
+bool GetList(SqList& L, int i, int& e)
+{
+	if (i<1 || i>L.Lengeth)
+		return false;
+	e = L.data[i - 1];
+}
+int main()
+{
+	int n = 5;
+	int i = 3;
+	int e;
+	SqList L;
+	InitList(L, n);
+	GetList(L, i, e);
+	printf("%d", e);
+	return 0;
+}
