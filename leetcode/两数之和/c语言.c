acd43de80639638de28b88b15551ddbd7ba42f13
c1529 <104741605+c1529@users.noreply.github.com>
2023-05-30 17:34:24 +0800

    Update c语言.c

--- leetcode/两数之和/c语言.c
+++ leetcode/两数之和/c语言.c
@@ -4,46 +4,36 @@
 解释：因为 nums[0] + nums[1] == 9 ，返回[0, 1] 。
 */
 #include<stdio.h>
-#include<malloc.h>
-int* ff(int nums[], int target)
+#include<stdlib.h>
+// 定义函数
+int* twoSum(int nums[], int numSize, int target, int* returnSize)
 {
-	int n = sizeof(nums) / sizeof(int);
-	int* ret = (int *)malloc(sizeof(int)*2);
-	for (int i = 0; i < n-1; i++)
-	{
-		for (int j = i+1; j < n; j++)
-		{
-			if (nums[i] + nums[j] == target)
-			{
-				ret[0] = i;
-				ret[1] = j;
-				return ret;
-			}
-		}
-	}
-	return NULL;
+    for (int i=0;i<numSize;i++)
+    {
+        for(int j=i+1;j<numSize;j++){
+            if(nums[i]+nums[j]==target){
+                int* ret = malloc(sizeof(int)*2);
+                ret[0] = i;
+                ret[1] = j;
+                *returnSize = 2;
+                return ret;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
 }
+
 int main()
 {
-	int nums[4] = { 2,7,11,15 };
-	int target = 9;
-	int* out;
-	out = ff(nums, target);
-	printf("%d %d", out[0], out[1]);
-	free(out);
-	return 0;
+    int arr1[4] = {1,5,6,9};
+    int target = 10;
+    int* returnSize;
+    int* ret;
+    ret =twoSum(arr1, 4, target,returnSize);
+    printf("数组的长度是：%d\n", *returnSize);
+    printf("第一个数的位置是:%d\n第二个数的位置是:%d", ret[0],ret[1]);
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
